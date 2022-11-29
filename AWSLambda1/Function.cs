f9d34c3ae3d8a26bcd54a8bb53929dc35152b197
marielmeza21 <mariel.meza@leasequery.com>
2022-11-29 00:58:55 -0500

    fix(test): this is hard
    COR-36589

--- AWSLambda1/Function.cs
+++ AWSLambda1/Function.cs
@@ -16,7 +16,7 @@ public class Function
     /// <returns></returns>
     public string FunctionHandler(string input, ILambdaContext context)
     {
-        Console.WriteLine("Test");
+        Console.WriteLine("Test 1");
         return input.ToUpper();
     }
 }
