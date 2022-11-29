de04fe1089f1d355f1647edb5bfb7bca5f66deb3
marielmeza21 <mariel.meza@leasequery.com>
2022-11-29 02:20:56 -0500

    feat(testing): testing release
    Testing if this commit creates a release
    
    BREAKING CHANGE: The creating of a release
    
    COR-36589

--- AWSLambda1/Function.cs
+++ AWSLambda1/Function.cs
@@ -16,7 +16,7 @@ public class Function
     /// <returns></returns>
     public string FunctionHandler(string input, ILambdaContext context)
     {
-        Console.WriteLine("Test");
+        Console.WriteLine("Test more");
         return input.ToUpper();
     }
 }
